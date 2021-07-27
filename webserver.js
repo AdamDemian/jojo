b0d27200962a426a2a45294ed4fba576ea298e11
AdamDemain <adamdemian1@gmail.com>
2021-07-27 21:03:15 +0200

    package error fix

--- webserver.js
+++ webserver.js
@@ -5,14 +5,13 @@ const port = process.env.PORT || 3000
 const path = require('path')
 const fs = require('fs')
 
-const ppm = require('jsonfile')
 
 // const rozvrhJson = '{"rozvrhJson" : "' + ppm.RTPPMDataMsgV1.rozvrhJson + '"}';
 
 // console.log(jsonfile.readFileSync(rozvrhJson))
 
 
-app.use('/dist', express.static(path.join(__dirname, 'dist')));
+app.use('/dist', express.static(path.join(__dirname, 'dist'))); 
 const rozvrhJson = app.use('/', express.static(path.join(__dirname, 'views')));
 
 app.set('view engine', 'ejs')
